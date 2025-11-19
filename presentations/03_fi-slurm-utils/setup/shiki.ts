b3f8dd4fce16bfd1b2e1edb0ee956430389b7836
Lehman Garrison <lgarrison@flatironinstitute.org>
2025-11-18 21:08:54 -0600

    03: first draft

---
+++ presentations/03_fi-slurm-utils/setup/shiki.ts
@@ -0,0 +1,14 @@
+/* ./setup/shiki.ts */
+import { defineShikiSetup } from '@slidev/types';
+
+export default defineShikiSetup(() => {
+  return {
+    themes: {
+      dark: 'github-dark-default',
+      light: 'github-light-default',
+    },
+    transformers: [
+      // ...
+    ],
+  };
+});
